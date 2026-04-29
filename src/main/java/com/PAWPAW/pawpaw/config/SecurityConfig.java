8b43818b4a2e83c35581de025d48c6b26ed46b4e
Maram <maram@gmail.com>
2026-04-29 17:59:46 +0300

    Fix security config and jwt filter requests

--- src/main/java/com/PAWPAW/pawpaw/config/SecurityConfig.java
+++ src/main/java/com/PAWPAW/pawpaw/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.PAWPAW.pawpaw.auth.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -85,6 +86,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/messages/**").permitAll()
                         .requestMatchers("/api/friends/**").permitAll()
                         .requestMatchers("/api/groups/**").permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/api/admin/**").hasAuthority("ROLE_ADMIN")
                         .anyRequest().authenticated()
                 )
