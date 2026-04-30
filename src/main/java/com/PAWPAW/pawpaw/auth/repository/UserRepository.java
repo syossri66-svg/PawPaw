94e321c111a4c6456c40e1324af90f9645e50a8a
Maram <maram@gmail.com>
2026-04-30 23:41:41 +0300

    Fix: Final resolution of Long vs UUID conflict in UserRepository

--- src/main/java/com/PAWPAW/pawpaw/auth/repository/UserRepository.java
+++ src/main/java/com/PAWPAW/pawpaw/auth/repository/UserRepository.java
@@ -8,11 +8,14 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, UUID> {
+
     Optional<User> findByEmail(String email);
+
     boolean existsByEmail(String email);
 
     long countByRole(UserRole userRole);
 
-    Optional<Object> findById(UUID id);
+
+
 }
