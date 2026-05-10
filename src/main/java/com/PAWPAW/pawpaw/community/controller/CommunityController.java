7c2e27af44ae8c818f1c604c35e6d2e6e37794ae
Maram <maram@gmail.com>
2026-05-10 22:57:46 +0300

    fix community endpoints

--- src/main/java/com/PAWPAW/pawpaw/community/controller/CommunityController.java
+++ src/main/java/com/PAWPAW/pawpaw/community/controller/CommunityController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/posts")
+@RequestMapping("/api/community")
 @RequiredArgsConstructor
 public class CommunityController {
 
