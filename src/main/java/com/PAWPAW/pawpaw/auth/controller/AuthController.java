18531b5819c57afce92fe05ae670300708aff332
Maram <maram@gmail.com>
2026-06-02 21:33:55 +0300

    fix: remove CrossOrigin from all controllers

--- src/main/java/com/PAWPAW/pawpaw/auth/controller/AuthController.java
+++ src/main/java/com/PAWPAW/pawpaw/auth/controller/AuthController.java
@@ -21,7 +21,6 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "*")
 public class AuthController {
 
     private final AuthService authService;
