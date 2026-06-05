2e3c608e80abdc28ee73cb8971fc96c3e56b2760
Maram <maram@gmail.com>
2026-06-05 23:54:49 +0300

    fix: resolve post image upload issue and integrate database saving

--- src/main/java/com/PAWPAW/pawpaw/config/SecurityConfig.java
+++ src/main/java/com/PAWPAW/pawpaw/config/SecurityConfig.java
@@ -20,7 +20,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.url.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
 
